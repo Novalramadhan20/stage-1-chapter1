c00107a34484687cbd127e5bf42386d2aeef81fb
Noval Ramadhan <macbookair@macbookairs-MacBook-Air.local>
2022-03-08 21:42:32 +0700

    https://github.com/Novalramadhan20/stage-1-chapter1

--- myproject.js
+++ myproject.js
@@ -5,9 +5,13 @@ function addProject() {
     let start = document.getElementById("start-date") .value;
     let end = document.getElementById("end-date").value;
     let desc = document.getElementById("description").value;
+    let nodeJS = document.getElementById("nodeJS").checked;
+    let nextJS = document.getElementById("nextJS").checked;
+    let reactJS = document.getElementById("reactJS").checked;
+    let typeScript = document.getElementById("typeScript").checked;
     let image = document.getElementById("input-project-image").files [0];
 
-    if (title == '' || start == '' || end == '' || desc == '' || image == ''){
+    if (title == '' || start == '' || end == '' || desc == '' ||image == ''){
         return alert ( 'All input fields must be not empty');
     }
 
@@ -21,6 +25,10 @@ function addProject() {
         start: startFull,
         end: endFull,
         desc: desc,
+        nodeJS: nodeJS,
+        nextJS: nextJS,
+        reactJS: reactJS,
+        typeScript: typeScript,
         image: image,       
     }
 
@@ -29,76 +37,66 @@ function addProject() {
     renderProject()
 
 }
+function getIcon(nodeJS, nextJS, reactJS, typeScript) {
+
+    let printIcon = ``
+    if (nodeJS == true) {
+        printIcon += `<img src="asset/nodejs.png" />` 
+    }
+    if (nextJS == true) {
+        printIcon += `<img src="asset/nextjs.svg" />` 
+    }
+    if (reactJS == true) {
+        printIcon += `<img src="asset/reactjs.png" />` 
+    }
+    if (typeScript == true) {
+        printIcon += `<img src="asset/typescript.png" />` 
+    }
+
+    return printIcon
+
+}
+
 
 function renderProject() {
 
-    let projectContainer = document.getElementById('contents')
+    let projectContainer = document.getElementById('contents') 
 
-    projectContainer.innerHTML =  
-    ` <div class="project-list" id="contents">
-    <div class="project-list-item"> 
-    <div class="project-image">
-        <img src="asset/mobileapp.jpeg" alt="mobileapp" />
-    </div>
-    <div class="project-content">
-        <h3>
-        <a href="project-detail.html" target="_blank"
-        >Dumbways Mobile App -2021</a>
-        </h3>
-    </div>    
-    <div class="detail-project-content">
-        <h5>durasi : 3 bulan
-        </h5>
-        <p>App that used for dumbways student, it was deployed and can downloaded on playstore. Happy download 
-        </p>
-    </div>    
-    <div class="project-technologies">
-    <div>
-        <img src="asset/playstore.png">
-        <img src="asset/android.png">
-        <img src="asset/java.png">
-    </div>
-    <div class="btn-group">
-        <button class="btn-edit">edit</button>
-        <button class="btn-delete">delete</button>
-    </div>
-    </div> `
+    projectContainer.innerHTML =  firstProjectContent();
+               
+    for (let i = 0; i < projects.length; i++)  {
 
-    for (let i = 0; i < projects.length;
-        i++)  {
-
-        projectContainer.innerHTML = 
-        ` <div class="project-list" id="contents"> 
-        <div id="${i}class="project-list-item">
-            <div class="project-image">
-                <img src="${projects[i].image}" alt="mobileapp" />
+        projectContainer.innerHTML += 
+       `<div id="${i}class="project-list" id="contents"> 
+            <div class="project-list-item">
+                <div class="project-image">
+                    <img src="${projects[i].image}" alt="mobileapp" />
+                </div>
+                <div class="project-content">
+                    <h3>
+                        <a href="project-detail.html" target="_blank"
+                        >${projects[i].title}</a>
+                    </h3>
+                </div>    
+                <div class="detail-project-content">
+                    <h5>durasi : ${getTimeDistance(projects[i].start, projects[i].end)}
+                    </h5>
+                    <p>${projects[i].desc}</p>
+                </div>    
+                <div class="project-technologies">
+                <div>
+                    <img ${getIcon(projects[i].nodeJS, projects[i].nextJS, projects[i].reactJS, projects[i].typeScript)}
+                </div>
+                <div class="btn-group">
+                            <a href="#" class="btn-edit">edit</a>
+                            <span></span>
+                            <a href="#" class="btn-delete">delete</a>
+                </div>
             </div>
-            <div class="project-content">
-                <h3>
-                    <a href="project-detail.html" target="_blank"
-                    >${projects[i].title}</a>
-                </h3>
-            </div>    
-            <div class="detail-project-content">
-                <h5>durasi : ${getTimeDistance(projects[i].end, projects[i].start)}
-                </h5>
-                <p>"${projects[i].desc} 
-                </p>
-            </div>    
-            <div class="project-technologies">
-            <div>
-                <img src="asset/playstore.png">
-                <img src="asset/android.png">
-                <img src="asset/java.png">
-            </div>
-            <div class="btn-group">
-                <button class="btn-edit" >edit</button>
-                <button class="btn-delete">delete</button>
-            </div>
-        </div>
-
         </div>   
-    `}
+        
+    `;
+}
 
 
 }
@@ -107,26 +105,57 @@ function getTimeDistance(start, end) {
 
 
     let distance = end - start
-    
+
     let yearDistance = Math.floor(distance / (12 * 4 * 7 * 24 * 60 * 60 * 1000))
+
     if (yearDistance != 0) {
-    return `${yearDistance} year`
+        return `${yearDistance} Tahun`
     } else {
         let monthDistance = Math.floor(distance / (4 * 7 * 24 * 60 * 60 * 1000))
-        if (monthDistance !=0) {
-        return `${monthDistance} month`
+        if (monthDistance != 0) {
+            return `${monthDistance} Bulan`
         } else {
             let weekDistance = Math.floor(distance / (7 * 24 * 60 * 60 * 1000))
-            if (weekDistance !=0) {
-            return `${weekDistance} week`
+            if (weekDistance != 0) {
+                return `${weekDistance} Minggu`
             } else {
-                let dayDistance =
-                Math.floor(distance / (24 * 60 * 60 * 1000))
-                if (dayDistance !=0) {
-                return `${dayDistance} day`
-                }
+                let dayDistance = Math.floor(distance / (24 * 60 * 60 * 1000))
+
+                return `${dayDistance} Hari`
             }
         }
     }
+    
+}
 
+function firstProjectContent() {
+   return `<div class="project-list" id="contents">
+                <div class="project-list-item"> 
+                    <div class="project-image">
+                        <img src="asset/mobileapp.jpeg" alt="mobileapp" />
+                    </div>
+                    <div class="project-content">
+                        <h3>
+                        <a href="project-detail.html" target="_blank"
+                        >Dumbways Mobile App -2021</a>
+                        </h3>
+                    </div>    
+                    <div class="detail-project-content">
+                        <h5>durasi : 3 bulan
+                        </h5>
+                        <p>App that used for dumbways student, it was deployed and can downloaded on playstore. Happy download 
+                        </p>
+                    </div>    
+                    <div class="project-technologies">
+                    <div>
+                        <img src="asset/playstore.png">
+                        <img src="asset/android.png">
+                        <img src="asset/java.png">
+                    </div>
+                    <div class="btn-group">
+                        <a href="#" class="btn-edit">edit</a>
+                        <span></span>
+                        <a href="#" class="btn-delete">delete</a>
+                    </div>
+                </div> `;
 }
